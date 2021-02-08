836a482c81fee786c034d6b7b5f97127ef210328
Sirat <sirat4757@gmail.com>
2021-02-08 13:39:01 +0600

    updated internals

---
+++ controllers/bootcamps.js
@@ -0,0 +1,35 @@
+
+// @desc GET all bootcamps
+// @route GET /api/v1/bootcamps
+// @access Public
+exports.getBootcamps = (req, res, next) => {
+    res.json({success: true})
+}
+
+// @desc GET single bootcamp
+// @route GET /api/v1/bootcamps/:id
+// @access Public
+exports.getBootcamp = (req, res, next) => {
+
+}
+
+// @desc Create a bootcamp
+// @route POST /api/v1/bootcamps
+// @access Private
+exports.createBootcamp = (req, res, next) => {
+
+}
+
+// @desc Update a bootcamp
+// @route PUT /api/v1/bootcamps/:id
+// @access Private
+exports.updateBootcamp = (req, res, next) => {
+
+}
+
+// @desc Delete a bootcamp
+// @route DELETE /api/v1/bootcamps/:id
+// @access Private
+exports.deleteBootcamp = (req, res, next) => {
+
+}
